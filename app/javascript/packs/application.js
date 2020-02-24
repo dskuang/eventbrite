7de546d3599b78f8fe927d228ac315fb756be4af
dominic_k <dominic_k@beezwax.net>
2020-02-24 10:24:01 -0800

    attempt to deploy

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,22 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require("vendor/phantom-js/breakpoints.min.js")
-require("vendor/phantom-js/browser.min.js")
-require("vendor/phantom-js/main.js")
-require("vendor/phantom-js/util.js")
-
-
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("jquery");
+require("vendor/phantom-js/breakpoints.min.js");
+require("vendor/phantom-js/browser.min.js");
+require("vendor/phantom-js/main.js");
+require("vendor/phantom-js/util.js");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
